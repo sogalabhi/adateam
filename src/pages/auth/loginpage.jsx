bff1e0b1608bdd34521b7ab7f53dc0e93cc6772d
ammufeezzz <ammarmufeez5@gmail.com>
2025-01-11 19:55:20 +0530

    authlogin

--- src/pages/auth/loginpage.jsx
+++ src/pages/auth/loginpage.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
-import { SupabaseClient } from '@supabase/supabase-js';
 import './LoginPage.css'; // Add a separate CSS file for styling
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://bcgvspkuazvdtmzaqyiw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjZ3ZzcGt1YXp2ZHRtemFxeWl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY1OTE5MDYsImV4cCI6MjA1MjE2NzkwNn0.WAcWP3VRdavS_in2IIaVFRvT-Lv7iDcFL3Aag__tUp4';
+
+// Initialize Supabase client
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const Login = () => {
   const [email, setEmail] = useState('');
